dfac626f6a5f1fc05cbe8db3a9ea0462ceca60dd
youdongkwan <dkyou7@naver.com>
2020-07-23 16:15:53 +0900

    예제 도메인 모델과 동작 확인

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,40 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"team"})
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")   // foreign key
+    private Team team;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public Member(String name, int age, Team team) {
+        this.name = name;
+        this.age = age;
+        if (team != null) {
+            setTeam(team);
+        }
+    }
+
+    public void setTeam(Team team){
+        this.team = team;
+        team.getMemberList().add(this);
+    }
+
+}
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import study.datajpa.Member;
+import study.datajpa.entity.Member;
 
 import static org.assertj.core.api.Assertions.*;
 
